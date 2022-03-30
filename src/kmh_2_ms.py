f2f2873cfc6995417394240004ba9641111dcb57
MaritzaB <amby.121192@gmail.com>
2022-03-30 13:52:27 -0700

    ✨ 🚗💨 Convert velocity from kmh to ms

---
+++ src/kmh_2_ms.py
@@ -0,0 +1,9 @@
+# Programa que lea en una variable una velocidad en km/h y la transforme a m/s. 
+
+print("Please enter velocity in km/h: \n")
+
+velocity_kmh = float(input())
+
+velocity_ms = velocity_kmh * (1000 / 3600)
+
+print(f"\n Velocity in m/s is: {velocity_ms} \n")
