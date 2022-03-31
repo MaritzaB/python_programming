03b6db8acb1820a8e492357643b3c5170f2d3234
MaritzaB <amby.121192@gmail.com>
2022-03-31 16:25:37 -0700

    ✨ Solve word_counter homework

--- src/count_your_phrase.py
+++ src/count_your_phrase.py
@@ -1,1 +1,7 @@
 # Un programa para contar el número de palabras en una frase.
+
+quote = str(input("Please enter you quote: \n"))
+
+n_words = len(quote.split())
+
+print(f"Your quote has {n_words} words \n")
