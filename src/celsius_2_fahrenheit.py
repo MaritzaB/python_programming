4d86e1fe8776b20557a8182664d4d14440cb1dfe
MaritzaB <amby.121192@gmail.com>
2022-03-31 22:21:34 -0700

    ⚡️ Turn program into function

--- src/celsius_2_fahrenheit.py
+++ src/celsius_2_fahrenheit.py
@@ -1,4 +1,4 @@
-# Programa que lea un dato de tipo float que contenga una cantidad de grados
+# 01 Programa que lea un dato de tipo float que contenga una cantidad de grados
 # centígrados para que mediante la aplicación de la fórmula F=32+(9*C/5),
 # convierta esa cantidad a grados Fahrenheit.
 
@@ -6,6 +6,8 @@ print("Please introduce the temperature in degrees Celsius: \n")
 
 degrees_celsius = float(input())
 
-degrees_fahrenheit = 32 + (9 * degrees_celsius / 5)
+def convert_celsius_to_fahrenheit(degrees_celsius):
+    degrees_fahrenheit = 32 + (9 * degrees_celsius / 5)
+    return(degrees_fahrenheit)
 
-print(f"The temperature in degrees fahrenheit is {degrees_fahrenheit}")
+print(f"The temperature in degrees fahrenheit is {convert_celsius_to_fahrenheit(degrees_celsius)}")
