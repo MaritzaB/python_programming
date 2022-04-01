432b29935a6007249508fa49cfc284981f22baee
MaritzaB <amby.121192@gmail.com>
2022-03-31 18:07:47 -0700

    ✨ solve split number

--- src/split_number.py
+++ src/split_number.py
@@ -1,3 +1,10 @@
 # Un programa que lea un número entero de 3 cifras y muestre por separado las
 # cifras de ese número
 
+while True:
+    number = str(input("\n Please, enter a number of 3 digits: \n"))
+    if len(number) != 3:
+        print("The number you've entered doesn't have 3 digits")
+    else:
+        print(f"The digits of the number are: {number[0]}, {number[1]} and {number[2]} \n")
+        break
