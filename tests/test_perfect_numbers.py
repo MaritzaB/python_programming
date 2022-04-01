21fa5dd20f636bf73481ee8ff52abc2d1854bf87
maritzee <amby.121192@gmail.com>
2022-04-01 14:43:32 -0700

    🧪 Add failing test

--- tests/test_perfect_numbers.py
+++ tests/test_perfect_numbers.py
@@ -1,6 +1,13 @@
 import pytest
 from ..src.perfect_numbers import *  # noqa
 
+def test_divide_number():
+    dividend = 6
+    divisor = 2
+    expected_output = 3
+    obtained_output = divide_number(dividend,divisor)
+    assert expected_output == obtained_output
+
 def test_get_perfect_numbers():
     starting_number = 1
     finishing_number = 30
