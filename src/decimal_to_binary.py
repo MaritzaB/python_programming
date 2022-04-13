900cba17b95aca4d965ba6747960cc92d5a253e6
maritzee <amby.121192@gmail.com>
2022-04-12 23:45:57 -0700

    ⚡️ Convert decimal to binary

--- src/decimal_to_binary.py
+++ src/decimal_to_binary.py
@@ -1,28 +1,23 @@
 # 07 Un programa para convertir un entero en decimal a binario en forma recursiva e
 # iterativa.
 
+number = 0
+lista=[number]
+while number>1:
+  quotient = int(number/2)
+  number = quotient
+  lista.append(number)
+  if quotient == 1:
+    break
+lista.reverse()
+print(*lista)
 
-"""
-# codigo de Cirse López
-import time
+binary_set = []
+for index,element in enumerate(lista):
+  if element % 2 == 0:
+    binary_set.append(0)
+  else:
+    binary_set.append(1)
 
-def dec_bin(n):
-  digitos = list() #Definimos una lista vacia
-  if n<2: #Caso trivial
-    digitos.append(n)
-  else :
-    a=n%2 #ultimo digito
-    digitos.append(a) #Lo guarda en la lista hasta el final
-    n=int((n-a)/2) # Calcula el numero sin el ultimo digito
-    digitos.extend(dec_bin(n)) #Junta los elementos de las listas
+print(*binary_set)
   
-  return(digitos)
-
-n = int(input("Numero decimal:> "))
-
-# Con funcion recursiva
-inicio_rec = time.time_ns()
-digitos = dec_bin(n)
-fin_rec = time.time_ns()
-di
-"""
