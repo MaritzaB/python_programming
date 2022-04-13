2d47e3910cd289761a4b1f5b787a5d59ee85a985
maritzee <amby.121192@gmail.com>
2022-04-12 17:54:11 -0700

    ✨ Solve perfect numbers

--- src/perfect_numbers.py
+++ src/perfect_numbers.py
@@ -2,3 +2,17 @@
 
 # Perfect number, a positive integer that is equal to the sum of its proper
 # divisors. The smallest perfect number is 6, which is the sum of 1, 2, and 3.
+
+def sum_its_divisors(number):
+    addition = 0
+    for divisor in range(1, number - 1):
+        if number % divisor == 0:
+            addition = addition + divisor
+    return addition
+
+def get_perfect_numbers(start,end):
+    perfect_numbers = []
+    for number in range(start,end):
+        if sum_its_divisors(number) == number:
+            perfect_numbers.append(number)
+    return(perfect_numbers)
