03c3d4637ddff5993a77017d5e4714f96507dda5
maritzee <amby.121192@gmail.com>
2022-04-12 22:27:23 -0700

    ⚡️ Calculate quotient iteratively

--- src/calculate_quotient.py
+++ src/calculate_quotient.py
@@ -1,2 +1,16 @@
 # 06 Un programa que pueda calcular el cociente de dos números enteros en forma
 # recursiva e iterativa.
+
+def divide_two_numbers(dividend,divisor):
+    pairs = []
+    while True:
+        quotient = int(dividend/divisor)
+        remainder = dividend % divisor
+        dividend = quotient
+        divisor = remainder
+        pairs.append([dividend,divisor])
+        if remainder == 0 or dividend<divisor:
+            break
+    return pairs
+
+print(divide_two_numbers(77,9))
